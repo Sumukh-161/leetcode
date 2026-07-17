89dd4c41e2569ee5586cb8ac3efcf4d7af625186
Sumukh-161 <sumukhadheeran@gmail.com>
2026-07-17 22:30:28 +0530

    Time: 4 ms (99.67%), Space: 48.1 MB (11.64%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean ispali(String s,int left,int right){
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1 ;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return ispali(s,left+1,right) || ispali(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
